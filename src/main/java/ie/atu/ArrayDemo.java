1e5897e322a869b6d0aa91ca1e8d19eab4ec7a27
Parthibg00410769 <g00410769@atu.ie>
2023-01-26 11:54:54 +0000

    q3

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -1,30 +1,43 @@
 package ie.atu;
+import java.util.Scanner;
+import java.sql.SQLOutput;
 
 public class ArrayDemo {
     public static void main(String[] args) {
-        int[] Array;
-        Array = new int[11];
-        Array[0]= 100;
-        Array[1]= 200;
-        Array[2]= 300;
-        Array[3]= 400;
-        Array[4]= 500;
-        Array[5]= 600;
-        Array[6]= 700;
-        Array[7]= 800;
-        Array[8]= 900;
-        Array[9]= 1000;
+        int[] anArray;
+        anArray = new int[10];
+        anArray[0]= 100;
+        anArray[1]= 200;
+        anArray[2]= 300;
+        anArray[3]= 400;
+        anArray[4]= 500;
+        anArray[5]= 600;
+        anArray[6]= 700;
+        anArray[7]= 800;
+        anArray[8]= 900;
+        anArray[9]= 1000;
+        System.out.println("Please enter your  number: ");
 
-        System.out.println("Element at index 0:" + Array[0]);
-        System.out.println("Element at index 1:" + Array[1]);
-        System.out.println("Element at index 2:" + Array[2]);
-        System.out.println("Element at index 3:" + Array[3]);
-        System.out.println("Element at index 4:" + Array[4]);
-        System.out.println("Element at index 5:" + Array[5]);
-        System.out.println("Element at index 6:" + Array[6]);
-        System.out.println("Element at index 7:" + Array[7]);
-        System.out.println("Element at index 8:" + Array[8]);
-        System.out.println("Element at index 9:" + Array[9]);
+        Scanner myScan = new Scanner(System.in);
+        anArray[10] = myScan.nextInt();
+
+        System.out.println("Element at index 0:" + anArray[0]);
+        System.out.println("Element at index 1:" + anArray[1]);
+        System.out.println("Element at index 2:" + anArray[2]);
+        System.out.println("Element at index 3:" + anArray[3]);
+        System.out.println("Element at index 4:" + anArray[4]);
+        System.out.println("Element at index 5:" + anArray[5]);
+        System.out.println("Element at index 6:" + anArray[6]);
+        System.out.println("Element at index 7:" + anArray[7]);
+        System.out.println("Element at index 8:" + anArray[8]);
+        System.out.println("Element at index 9:" + anArray[9]);
+        System.out.println("Element at index 10: " + anArray[10]);
+
+
+
+
+
+
 
 
 
