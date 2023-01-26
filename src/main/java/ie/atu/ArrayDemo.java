a816c7ee40249fa8b0a57aa797e4f4c00f7e7b71
Parthibg00410769 <g00410769@atu.ie>
2023-01-26 11:30:57 +0000

    q1

---
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -0,0 +1,36 @@
+package ie.atu;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] Array;
+        Array = new int[11];
+        Array[0]= 100;
+        Array[1]= 200;
+        Array[2]= 300;
+        Array[3]= 400;
+        Array[4]= 500;
+        Array[5]= 600;
+        Array[6]= 700;
+        Array[7]= 800;
+        Array[8]= 900;
+        Array[9]= 1000;
+
+        System.out.println("Element at index 0:" + Array[0]);
+        System.out.println("Element at index 1:" + Array[1]);
+        System.out.println("Element at index 2:" + Array[2]);
+        System.out.println("Element at index 3:" + Array[3]);
+        System.out.println("Element at index 4:" + Array[4]);
+        System.out.println("Element at index 5:" + Array[5]);
+        System.out.println("Element at index 6:" + Array[6]);
+        System.out.println("Element at index 7:" + Array[7]);
+        System.out.println("Element at index 8:" + Array[8]);
+        System.out.println("Element at index 9:" + Array[9]);
+
+
+
+
+
+
+
+    }
+}
